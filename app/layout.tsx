134c9b4c0293718cc973f4ae5f82977f405592ee
olradarsverige-ops <olradar.sverige@gmail.com>
2025-11-10 23:17:01 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import './styles/cta-amber.css';
 import type { Metadata } from 'next'
 import Providers from '../components/providers'
 import { ToggleTheme } from '../components/ui'
